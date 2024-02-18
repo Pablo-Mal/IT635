764904e01f2fa165b7bf60e67f3904fe6777499b
Pablo-Mal <54961754+Pablo-Mal@users.noreply.github.com>
2024-02-17 20:30:27 -0500

    Delete test3/script.sh

--- test3/script.sh
+++
@@ -1,9 +0,0 @@
-sudo -u postgres psql
-pip3 install psycopg2-binary
-sudo apt-get update
-wget https://raw.githubusercontent.com/Pablo-Mal/IT635/main/test3/allinOne.sql
-wget https://github.com/Pablo-Mal/IT635/blob/main/test3/checkInv.py
-wget https://github.com/Pablo-Mal/IT635/blob/main/test3/insertAllinOne.sql
-sudo -u postgres psql < allinOne.sql
-sudo -u postgres psql < insertAllinOne.sql
-python3 ./checkInv.py
