d1a8523e9d05b3600d8a1a456040be35ad878685
Pablo-Mal <54961754+Pablo-Mal@users.noreply.github.com>
2024-02-17 20:34:01 -0500

    Update script.sh

--- test3/script.sh
+++ test3/script.sh
@@ -4,5 +4,6 @@ sudo apt-get update
 wget https://raw.githubusercontent.com/Pablo-Mal/IT635/main/test3/allinOne.sql | sleep 3
 wget https://raw.githubusercontent.com/Pablo-Mal/IT635/main/test3/checkInv.py | sleep 3
 wget https://raw.githubusercontent.com/Pablo-Mal/IT635/main/test3/insertAllinOne.sql | sleep 3
+wget https://raw.githubusercontent.com/Pablo-Mal/IT635/main/test3/script2.sh | sleep 3
 
 bash script2.sh
