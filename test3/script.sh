78af8bebfac7fda11df46b7de658c2e3e1a5d10e
Pablo-Mal <54961754+Pablo-Mal@users.noreply.github.com>
2024-02-24 11:09:20 -0500

    Update script.sh

--- test3/script.sh
+++ test3/script.sh
@@ -1,7 +1,6 @@
-sudo -u postgres psql
 pip3 install psycopg2-binary
 sudo apt-get update
 wget https://raw.githubusercontent.com/Pablo-Mal/IT635/main/test3/allinOne.sql | sleep 3
 wget https://raw.githubusercontent.com/Pablo-Mal/IT635/main/test3/checkInv.py | sleep 3
 wget https://raw.githubusercontent.com/Pablo-Mal/IT635/main/test3/insertAllinOne.sql | sleep 3
-wget https://raw.githubusercontent.com/Pablo-Mal/IT635/main/test3/script2.sh | sleep 3
+wget https://raw.githubusercontent.com/Pablo-Mal/IT635/main/test3/script2.sh
